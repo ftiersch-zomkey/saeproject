29f5654c457abda54b1c73e2c34d8044eb83b33c
zettlrobert <zettl_robert@hotmail.de>
2016-06-16 17:08:37 +0200

    delete c&p

--- comments.php
+++ comments.php
@@ -1,37 +1,6 @@
 
-      <div id="kommentar_formular">
+<div id="kommentar_formular">
 
-<h3 id="respond">Kommentar schreiben</h3>
-
-<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
-
-   <p>
-      <input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" />
-      <label for="author">Name</label>
-   </p>
-
-   <p>
-      <input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" />
-      <label for="email">Email <small>(wird nicht veröffentlicht)</small></label>
-   </p>
-
-   <p>
-      <input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
-      <label for="url">Webseite</label>
-   </p>
-
-   <p>
-      Kommentar
-      <textarea name="comment" id="comment" style="width: 100%;" rows="10" tabindex="4"></textarea>
-   </p>
-
-   <p>
-      <input name="submit" type="submit" id="submit" tabindex="5" value="Kommentar abschicken" />
-      <input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
-   </p>
-
-   <?php do_action('comment_form', $post->ID); ?>
-
-</form>
+SELECT * FROM comments; 
 
 </div> <!-- kommentar_formular -->
