1353d015b60e16bfe16ec212d2df87bb5d540138
zettlrobert <zettl_robert@hotmail.de>
2016-06-16 16:33:43 +0200

    Create comments.php

---
+++ comments.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>comments</title>
+  </head>
+  <body>
+    <form class="comments" action="index.html" method="post">
+      
+
+    </form>
+  </body>
+</html>
