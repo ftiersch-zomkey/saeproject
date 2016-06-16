076512d931afb33aa2a4b2998237d7149ecffd98
gabrielduhoux <gabriel.duhoux@gmail.com>
2016-06-16 17:51:15 +0200

    title test

--- details.php
+++ details.php
@@ -4,7 +4,7 @@ require_once("config/mysql.php");
 require_once("config/functions.php");
 
 
-$news_result = mysqli_query($conn, "SELECT * FROM news");
+$news_result = mysqli_query($conn, "SELECT * FROM news WHERE id='$_GET['id']'");
 $test = mysqli_fetch_assoc($news_result);
 
 print_r($test);
@@ -30,7 +30,7 @@ print_r($test);
     <h1>Details - News</h1>
 
     <div class="main_content">
-      <?php // echo $news_result; ?>
+      <?php echo $test['title']; ?>
     </div>
 
     <div class="side_container">
