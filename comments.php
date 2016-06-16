9d65e11a7f0661b025c373c77dc7a6fa28edfdaa
zettlrobert <zettl_robert@hotmail.de>
2016-06-16 17:54:17 +0200

    p tt

--- comments.php
+++ comments.php
@@ -7,5 +7,7 @@ $conncomments = mysqli_query($conn, "SELECT * FROM comments");
 while($comment = mysqli_fetch_assoc($conncomments))
         echo $comment["id"];
  ?>
-
+<p>
+  test
+</p>
 </div> <!-- kommentar_formular -->
