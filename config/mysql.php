fc4085966999a4befd873d15b2beeffe57c85f58
Frank Tiersch Extern <ftiersch.extern@chip.de>
2016-06-16 13:01:15 +0200

    Added mysql connection file

---
+++ config/mysql.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect("localhost", "sae", "sae", "sae");
+if (mysqli_connect_error()) {
+	exit(mysqli_connect_error());
+}
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once("config/mysql.php");
+
+
+mysqli_close($conn);
+?>
