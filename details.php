a8987ad7fcc7724fcfa2979d53caf8c2596bd3e5
gabrielduhoux <gabriel.duhoux@gmail.com>
2016-06-16 17:53:16 +0200

    erneut

--- details.php
+++ details.php
@@ -4,7 +4,7 @@ require_once("config/mysql.php");
 require_once("config/functions.php");
 
 
-$news_result = mysqli_query($conn, "SELECT * FROM news WHERE id='$_GET['id']'");
+$news_result = mysqli_query($conn, "SELECT * FROM news WHERE id=" .$_GET['id']);
 $test = mysqli_fetch_assoc($news_result);
 
 print_r($test);
