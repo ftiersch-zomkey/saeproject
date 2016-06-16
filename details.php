a460800bb2cdc6cd945c6b68c3097ca5717da0cd
gabrielduhoux <gabriel.duhoux@gmail.com>
2016-06-16 16:43:09 +0200

    first image

--- details.php
+++ details.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once("comments.php");
+include_once("comments.php", "details.css");
 
 
 ?>
@@ -11,8 +11,30 @@ include_once("comments.php");
   <head>
     <meta charset="utf-8">
     <title>Details</title>
+    <!--  <link rel="stylesheet" href="details.css" media="screen" charset="utf-8">  -->
+
+
   </head>
   <body>
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="#">Politics</a>
+      <a href="#">Sports</a>
+    </nav>
+    <h1>Details - News</h1>
+
+    <div class="main_content">
+      <?php echo $news_result; ?>
+    </div>
+
+    <div class="side_container">
+
+    </div>
 
   </body>
+  <footer>
+    <a href="#">Impressum</a>
+    <a href="http://facebook.com" target="_blank">Facebook</a>
+    <a href="http://twitter.com" target="_blank">Twitter</a>
+  </footer>
 </html>
